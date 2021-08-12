49351cdf656369ac113a124f419b085126967b17
McLutzifer <lukasschumi@gmx.at>
2021-08-12 17:48:22 +0200

    create "normal" function

---
+++ udemy/next-gen-JS/arrowFunctions.js
@@ -0,0 +1,16 @@
+// diferent syntax for creating functions
+
+function myFnc1() {
+    console.log("1");
+}
+
+const myFnc2 = () => {
+    console.log("2");
+}
+
+
+function printMyName(name) {
+    console.log(name);
+}
+
+printMyName('Max');
