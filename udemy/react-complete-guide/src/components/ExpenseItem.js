8dce27af6c27aacd6bf5233783dd7dfedae5dd10
McLutzifer <lukasschumi@gmx.at>
2021-08-15 15:49:08 +0200

    add const Date

--- udemy/react-complete-guide/src/components/ExpenseItem.js
+++ udemy/react-complete-guide/src/components/ExpenseItem.js
@@ -1,6 +1,8 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    
     return (
         <div className="expense-item">
             <div>Date</div>
