330baac6a7b9dd695a37e465aa660f670c0ef2ea
McLutzifer <lukasschumi@gmx.at>
2021-08-17 20:36:19 +0200

    update

--- udemy/next-gen-JS/SpreadAndRestOperators.js
+++ udemy/next-gen-JS/SpreadAndRestOperators.js
@@ -1,12 +1,28 @@
 // Spread
-const oldArray = [1, 2, 3, 4, 5]
-
-const newArray = [...oldArray, 6, 7]
+const oldArray = [1, 2, 3, 4, 5];
+const newArray = [...oldArray, 6, 7];
 
 console.log(newArray)
 
+//object
+const person = {
+    name: 'Max'
+};
+
+const newPerson = {
+    ...person,
+    age:28
+}
+
+console.log(newPerson)
 
 // Rest
 function sortArgs(...args) {
     return args.sort()
 }
+
+const filter = (...args) => {
+    return args.filter(el => el === 1);
+}
+
+console.log(filter(1, 2, 3))
