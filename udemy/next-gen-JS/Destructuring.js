bfb552545767d631a215b6ca738fd0cbda46097a
McLutzifer <lukasschumi@gmx.at>
2021-08-17 20:41:41 +0200

    destructuring an array

--- udemy/next-gen-JS/Destructuring.js
+++ udemy/next-gen-JS/Destructuring.js
@@ -1,5 +1,9 @@
 [a, b] = ['Hello', 'Max']
 console.log(a);
+//{newName} = {newName: 'Max', age: 28}
+//console.log(newName) 
 
-{newName} = {newName: 'Max', age: 28}
-console.log(newName) 
+const numbers = [1, 2, 3];
+
+[num1, num2] = numbers;
+console.log(num1, num2)
