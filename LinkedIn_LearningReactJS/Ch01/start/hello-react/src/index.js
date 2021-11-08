501b065ae0e840abd8f0a32639082a672c446b9f
McLutzifer <lukasschumi@gmx.at>
2021-11-08 18:51:02 +0100

    dot notation

--- LinkedIn_LearningReactJS/Ch01/start/hello-react/src/index.js
+++ LinkedIn_LearningReactJS/Ch01/start/hello-react/src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+let city = {
+  name: "Madrid",
+  country: "Spain"
+};
 
 ReactDOM.render(
-  React.createElement("h1", null, "Hello!"),
+  <h1>{city.name} is in {city.country}</h1>,
   document.getElementById('root')
 );
 
