f2231d8719aeba16fa6b14028f3313e84be936b2
McLutzifer <lukasschumi@gmx.at>
2021-11-09 15:36:14 +0100

    update to VM ubuntu

--- LinkedIn_LearningReactJS/Ch01/start/hello-react/src/index.js
+++ LinkedIn_LearningReactJS/Ch01/start/hello-react/src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Hello() {
+  return (
+   <div>
+    <h1>Welcome to React</h1>
+    <p>Let's build something cool.</p>
+    </div>
+  )
+    }
+
 let city = {
   name: "Madrid",
   country: "Spain"
 };
 
 ReactDOM.render(
-  <h1>{city.name} is in {city.country}</h1>,
+  <Hello></Hello>,
+  
   document.getElementById('root')
 );
-
-
