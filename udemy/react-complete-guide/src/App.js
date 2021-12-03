e77f5f0f88d0e220971faa02586e3bfc747e2991
McLutzifer <lukasschumi@gmx.at>
2021-12-03 14:22:11 +0100

    using props

--- udemy/react-complete-guide/src/App.js
+++ udemy/react-complete-guide/src/App.js
@@ -3,9 +3,9 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     { title: "Car Inssurance", amount: 284.67, date: new Date(2021, 2, 28) },
-    { title: "Car Auto", amount: 2.14, date: new Date(2021, 3, 22) },
-    { title: "Car Bett", amount: 27.11, date: new Date(2021, 9, 16) },
-    { title: "Baes Inssurance", amount: 287, date: new Date(2021, 13, 11) },
+    { title: "Alien Farm", amount: 2.14, date: new Date(2021, 3, 22) },
+    { title: "Car Bed", amount: 27.11, date: new Date(2021, 9, 16) },
+    { title: "Baer Inssurance", amount: 287, date: new Date(2021, 13, 11) },
   ];
 
   return (
@@ -17,9 +17,21 @@ function App() {
         amount={expenses[0].amount}
         date={expenses[0].date}
       ></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
