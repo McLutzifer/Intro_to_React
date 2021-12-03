190a9d2ee1b47194df6de0ab37bb92271846b0ab
McLutzifer <lukasschumi@gmx.at>
2021-12-03 14:15:16 +0100

    autoformat

--- udemy/react-complete-guide/src/App.js
+++ udemy/react-complete-guide/src/App.js
@@ -2,23 +2,26 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
-    { title: 'Car Inssurance', amount: 284.67, date: new Date(2021, 2, 28)},
-    { title: 'Car Auto', amount: 2.14, date: new Date(2021, 3, 22)},
-    { title: 'Car Bett', amount: 27.11, date: new Date(2021, 9, 16)},
-    { title: 'Baes Inssurance', amount: 287, date: new Date(2021, 13, 11)}
+    { title: "Car Inssurance", amount: 284.67, date: new Date(2021, 2, 28) },
+    { title: "Car Auto", amount: 2.14, date: new Date(2021, 3, 22) },
+    { title: "Car Bett", amount: 27.11, date: new Date(2021, 9, 16) },
+    { title: "Baes Inssurance", amount: 287, date: new Date(2021, 13, 11) },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible</p>
-        <ExpenseItem title={expenses[0].title}></ExpenseItem>
-        <ExpenseItem></ExpenseItem>
-        <ExpenseItem></ExpenseItem>
-        <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
-
 
 export default App;
