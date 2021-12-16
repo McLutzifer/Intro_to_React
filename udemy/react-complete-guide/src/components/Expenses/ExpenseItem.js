2a53a2607eb8a21e5dea55171b8f2b3230418b16
McLutzifer <lukasschumi@gmx.at>
2021-12-16 12:55:40 +0100

    reevaluate components

--- udemy/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ udemy/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -3,14 +3,16 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const clickHandler = () => {
-    console.log('Clicked!!!!!!');
-  }
 
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = 'Updated!';
+  };
 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
@@ -18,6 +20,6 @@ const ExpenseItem = (props) => {
       <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
