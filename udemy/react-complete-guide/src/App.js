fdd1a0c23b0af8f9d91411a977dcd1538f2d3bb3
McLutzifer <lukasschumi@gmx.at>
2021-12-16 11:33:52 +0100

    arrow function

--- udemy/react-complete-guide/src/App.js
+++ udemy/react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     { title: "Car Inssurance", amount: 284.67, date: new Date(2021, 2, 28) },
     { title: "Alien Farm", amount: 2.14, date: new Date(2021, 3, 22) },
@@ -16,5 +16,24 @@ function App() {
     </div>
   );
 }
+
+
+// function App() {
+//   const expenses = [
+//     { title: "Car Inssurance", amount: 284.67, date: new Date(2021, 2, 28) },
+//     { title: "Alien Farm", amount: 2.14, date: new Date(2021, 3, 22) },
+//     { title: "Car Bed", amount: 27.11, date: new Date(2021, 9, 16) },
+//     { title: "Baer Inssurance", amount: 287, date: new Date(2021, 13, 11) },
+//   ];
+
+//   return (
+//     <div>
+//       <h2>Let's get started!</h2>
+//       <p>This is also visible</p>
+//       <Expenses item={expenses} />
+//     </div>
+//   );
+// }
+
 
 export default App;
