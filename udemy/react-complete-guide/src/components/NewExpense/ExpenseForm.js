cd93b7c708e143e14962aee38b4c30702103df27
McLutzifer <lukasschumi@gmx.at>
2021-12-21 17:13:56 +0100

    add button

--- udemy/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ udemy/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -13,11 +13,14 @@ const ExpenseForm = () => {
         <div className="new-expense__control">
           <label>Amount</label>
           <input type="number" min="0.01" step="0.01" />
-          </div>
-          <div className="new-expense__control">
+        </div>
+        <div className="new-expense__control">
           <label>Date</label>
           <input type="date" min="2019-01-01" step="2022-12-31" />
-          </div>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+          <button type='submit'>Add Expense</button>
       </div>
     </form>
   );
