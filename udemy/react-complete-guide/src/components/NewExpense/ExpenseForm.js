35b88baf99111b6fc9b1f358e643e46f64e21263
McLutzifer <lukasschumi@gmx.at>
2021-12-21 17:10:17 +0100

    add form

--- udemy/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ udemy/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,5 +1,18 @@
-import React from "react"
+import React from "react";
 
-const ExpenseForm = () => {};
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+      </div>
+    </form>
+  );
+};
 
 export default ExpenseForm;
