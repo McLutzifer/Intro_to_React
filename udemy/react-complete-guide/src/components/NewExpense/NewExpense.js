896ff5889970c44de3ed2e6caaf5be25da772950
McLutzifer <lukasschumi@gmx.at>
2021-12-21 17:14:51 +0100

    add NewExpenseFor component

--- udemy/react-complete-guide/src/components/NewExpense/NewExpense.js
+++ udemy/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,12 +1,11 @@
 import React from "react";
 
+import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 const NewExpense = () => {
     return <div className="new-expense">
-        <form>
-
-        </form>
+        <NewExpense />
     </div>
 };
 
