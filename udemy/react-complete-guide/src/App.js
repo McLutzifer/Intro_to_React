0cbaaef72187c6bef9150b20df3c334c291ed602
McLutzifer <lukasschumi@gmx.at>
2021-12-21 17:16:23 +0100

    render NewExpense

--- udemy/react-complete-guide/src/App.js
+++ udemy/react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -12,8 +13,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible</p>
+      <NewExpense />
       <Expenses item={expenses} />
     </div>
   );
--- udemy/react-complete-guide/src/components/NewExpense/NewExpense.js
+++ udemy/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -4,9 +4,11 @@ import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 const NewExpense = () => {
-    return <div className="new-expense">
+    return 
+    (<div className="new-expense">
         <NewExpense />
     </div>
+    );
 };
 
 export default NewExpense;
