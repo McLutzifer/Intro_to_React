47891520949542ca7cfa9f79156f705028bf2d67
McLutzifer <lukasschumi@gmx.at>
2021-12-22 17:40:07 +0100

    format

--- udemy/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ udemy/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -20,7 +20,7 @@ const ExpenseForm = () => {
         </div>
       </div>
       <div className="new-expense__actions">
-          <button type='submit'>Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
