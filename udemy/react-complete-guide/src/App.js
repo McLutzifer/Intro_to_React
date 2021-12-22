8a5da965dd591a26fab7b41082f6552343876bca
McLutzifer <lukasschumi@gmx.at>
2021-12-22 17:40:33 +0100

    delete old funnction

--- udemy/react-complete-guide/src/App.js
+++ udemy/react-complete-guide/src/App.js
@@ -20,22 +20,4 @@ const App = () => {
 }
 
 
-// function App() {
-//   const expenses = [
-//     { title: "Car Inssurance", amount: 284.67, date: new Date(2021, 2, 28) },
-//     { title: "Alien Farm", amount: 2.14, date: new Date(2021, 3, 22) },
-//     { title: "Car Bed", amount: 27.11, date: new Date(2021, 9, 16) },
-//     { title: "Baer Inssurance", amount: 287, date: new Date(2021, 13, 11) },
-//   ];
-
-//   return (
-//     <div>
-//       <h2>Let's get started!</h2>
-//       <p>This is also visible</p>
-//       <Expenses item={expenses} />
-//     </div>
-//   );
-// }
-
-
 export default App;
