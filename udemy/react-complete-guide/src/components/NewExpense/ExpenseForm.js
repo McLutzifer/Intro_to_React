cc4290886ef26729c3da45d6c4f6026198f458de
McLutzifer <lukasschumi@gmx.at>
2022-01-05 14:46:30 +0100

    add titlechangehandler

--- udemy/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ udemy/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const titleChangeHandler = () => {
-    console.log('Title Changed')
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
   };
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
 
   return (
     <form>
