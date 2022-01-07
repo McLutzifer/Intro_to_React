60dd5b53db3294a8e7968cb77c1393ff73c5912d
McLutzifer <lukasschumi@gmx.at>
2022-01-07 12:49:45 +0100

    saveExpenseDataHandler

--- udemy/react-complete-guide/src/components/NewExpense/NewExpense.js
+++ udemy/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -6,12 +6,19 @@ import "./NewExpense.css";
 
 
 const NewExpense = () => {
+  const SaveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    console.log(expenseData);
+  };
 
-  
+
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData/>
+      <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler}/>
     </div>
   );
 };
